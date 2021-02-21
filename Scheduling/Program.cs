d262861dd90f3122650aa74174719d743d7129f9
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-21 23:45:33 +0200

    Add files via upload

---
+++ Scheduling/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Scheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> tasks = new Stack<int>();
+            
+            int[] data = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            foreach (int element in data)
+            {
+                tasks.Push(element);
+            }
+
+
+            Queue<int> threads = new Queue<int>();
+            
+            data = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            foreach (int element in data)
+            {
+                threads.Enqueue(element);
+            }
+
+            int cpuTask = int.Parse(Console.ReadLine());
+
+            int curentTask = 0;
+            int curentThread = 0;
+            while (true)
+            {
+                curentTask = tasks.Pop();
+                curentThread = threads.Peek();
+
+                if(curentTask == cpuTask)
+                {
+                    break;
+                }
+
+                if(curentThread >= curentTask)
+                {
+                    threads.Dequeue();
+                }
+                else
+                {
+                    threads.Dequeue();
+                    tasks.Push(curentTask);
+                }
+            }
+
+            Console.WriteLine($"Thread with value {curentThread} killed task {cpuTask}");
+
+            Console.WriteLine(string.Join(' ', threads));
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
