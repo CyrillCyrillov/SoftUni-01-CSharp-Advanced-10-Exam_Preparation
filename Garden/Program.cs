06d984123a2dd29f83065978ef962d787a8df085
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-21 23:53:16 +0200

    Add files via upload

---
+++ Garden/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace Garden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimentions = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rows = dimentions[0];
+            int columns = dimentions[1];
+
+            int[,] garden = new int[rows, columns];
+
+            for (int i = 0; i <= rows - 1; i++)
+            {
+                for (int j = 0; j <= columns - 1; j++)
+                {
+                    garden[i, j] = 0;
+                }
+
+            }
+
+            string comand = null;
+            while ((comand = Console.ReadLine()) != "Bloom Bloom Plow")
+            {
+                int[] newFlower = comand.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                int curentRow = newFlower[0];
+                int curentColumn = newFlower[1];
+
+                if(curentRow < 0 || curentRow > rows - 1 || curentColumn < 0 || curentColumn > columns - 1)
+                {
+                    Console.WriteLine("Invalid coordinates.");
+                    continue;
+                }
+
+                for (int i = 0; i <= columns - 1; i++)
+                {
+                    garden[curentRow, i]++;
+                }
+                for (int i = 0; i <= rows - 1; i++)
+                {
+                    garden[i, curentColumn]++;
+                }
+            }
+
+
+            for (int i = 0; i <= rows - 1; i++) // ********* ROWS
+            {
+                for (int j = 0; j <= columns -1; j++) // ****** COLUMNS
+                {
+                    Console.Write(garden[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
